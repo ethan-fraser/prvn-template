a4a7451f68348f320386700d7062ecb21c8b6350
Ethan Fraser <ejpfraser@gmail.com>
2025-02-24 22:07:53 +1300

    initial commit

---
+++ src/lib/pocketbase.ts
@@ -0,0 +1,14 @@
+import Pocketbase from "pocketbase";
+
+const pb = new Pocketbase(import.meta.env.VITE_POCKETBASE_BASE_URL);
+
+export default class PocketbaseService {
+  static async checkConnection() {
+    try {
+      const result = await pb.health.check();
+      return result.code === 200;
+    } catch {
+      return false;
+    }
+  }
+}
